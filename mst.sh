90d3f445acb1c0f8c692d775a615965fa10d0d8f
konstin <konstin@mailbox.org>
2022-04-06 00:40:12 +0200

    no pyo3 feature by default

--- mst.sh
+++ mst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-maturin build --release --strip -i python
+maturin build --release --strip -i python --cargo-extra-args="--features=python_bindings"
 zip -ur target/wheels/virtual_sprawl-0.1.0-cp37-abi3-manylinux_2_31_x86_64.whl load_virtual_sprawl.pth
 .venv/bin/pip uninstall -y -q virtual-sprawl
 .venv/bin/pip install -q target/wheels/virtual_sprawl-0.1.0-cp37-abi3-manylinux_2_31_x86_64.whl
