29193ebb33d7df742bb865e52fc1e7bb83fa9104
konstin <konstin@mailbox.org>
2022-04-13 17:22:02 +0200

    fix test wheel location

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -9,7 +9,7 @@ cargo build
 cargo test
 python -m test.compare popular-wheels/tqdm-4.62.3-py2.py3-none-any.whl
 python -m test.test_tqdm
-python -m test.compare wheels/purelib_and_platlib-1.0.0-cp38-cp38-linux_x86_64.whl
+python -m test.compare test-data/wheels/purelib_and_platlib-1.0.0-cp38-cp38-linux_x86_64.whl
 python -m test.test_piptests
 python -m test.test_popular
 python -m test.compare_poetry data_science_project -E tqdm_feature
